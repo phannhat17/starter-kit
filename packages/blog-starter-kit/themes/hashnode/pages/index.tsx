85ef72adcfe08ed2d0c98ebda4de2aa996dd59b4
Phan Nhat <52977403+phannhat17@users.noreply.github.com>
2024-02-08 12:07:32 +0000

    fix spacing

--- packages/blog-starter-kit/themes/hashnode/pages/index.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/index.tsx
@@ -136,7 +136,7 @@ export default function Index(
 					/>
 				</Head>
 				<Header isHome={true} />
-				<div className="scroll-smooth dark:bg-slate-900">
+				<div className="scroll-smooth dark:bg-slate-900 mb-2">
 					{postsToBeRendered.edges.length > 0 ? (
 						<FeaturedPosts
 							posts={postsToBeRendered.edges.map((p: any) => p.node).slice(0, 3)}
